ed1ed4a49fa4eba58958699c99eeb0f8ee189df2
UnluckyForSome <30089567+UnluckyForSome@users.noreply.github.com>
2018-09-11 10:42:05 +0100

    Update

--- 7zEmuPrepper.ps1
+++ 7zEmuPrepper.ps1
@@ -14,8 +14,15 @@ param (
 "Keep Extracted?: $KeepExtracted"
 ""
 
+# Example commands:
 # PCSX2 = """--nogui"""
 # RetroArch = """"-L""" """C:/Program Files/RetroArch/cores/dolphin_libretro.dll""""
+
+# Playstation with Beetle PSX RetroArch
+# C:\Users\Joe\Documents\McWork\7zPrepper.exe "C:\PROGRA~1\7-Zip\7z.exe" "C:\PROGRA~1\RetroArch\retroarch.exe" "-L C:\PROGRA~1\RetroArch\cores\mednafen_psx_hw_libretro.dll -f" "D:\PS1\American Pool (USA).zip"
+
+# Playstation 2 with PCSX2
+# C:\Users\Joe\Documents\McWork\7zPrepper.exe "C:\PROGRA~1\7-Zip\7z.exe" "C:\PROGRA~2\PCSX2\pcsx2.exe" """--nogui""" "D:\PS2\Chess Challenger (Europe) (En,Fr,De,Es,It).7z"
 
 # Define the individual filename without the rest of the path
 $fileName = [System.IO.Path]::GetFileNameWithoutExtension("$filePath")
@@ -24,11 +31,25 @@ $fileName = [System.IO.Path]::GetFileNameWithoutExtension("$filePath")
 "Starting decompression of [$filePath]..."
 & $7ZipPath "e" $filePath "-aos" | Out-Null
 
-# Define the file we want the emulator to load by searching the individual filename and looking for common extensions
-"Looking for playable file..."
-$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+# Perform a general pass of filetypes to try and launch if emulator is not identified in the following steps
+"RetroArch Matched"
+$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
 
-# Start emulator
+# Identify PCSX2 as the emulator and search for the right type of files to launch
+if ($emulatorPath -Match 'pcsx2.exe')
+{
+"PCSX2 Matched"
+$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+}
+
+# Identify RetroArch as the emulator and search for the right type of files to launch
+if ($emulatorPath -Match 'retroarch.exe')
+{
+"RetroArch Matched"
+$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+}
+
+# Launch emulator
 "Launching emulator with [$extractedFile]..."
 Start-Process $emulatorPath -ArgumentList $emulatorArguments, """$extractedFile""" -Wait
 
@@ -36,7 +57,7 @@ Start-Process $emulatorPath -ArgumentList $emulatorArguments, """$extractedFile"
 if ($KeepExtracted)
 { 
     "Process Complete!"
-    exit 
+    exit
 }
 
 # If "KeepExtracted" argument is not passed, remove all of the extracted files when emulator closes
