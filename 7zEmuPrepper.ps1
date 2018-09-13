9b461f5c39de23273625f88c9ae3fc75eca505a6
UnluckyForSome <30089567+UnluckyForSome@users.noreply.github.com>
2018-09-13 13:42:52 +0100

    Update

--- 7zEmuPrepper.ps1
+++ 7zEmuPrepper.ps1
@@ -1,8 +1,15 @@
-param (    
+param (
+    [Parameter(Mandatory=$True)]
     [string]$7ZipPath, # File location of the 7Zip exe
+    [Parameter(Mandatory=$True)]
     [string]$emulatorPath, # File location of the Emulator exe
+    [Parameter(Mandatory=$True)]
     [string]$emulatorArguments, # Arguments for the Emulator
+    [Parameter(Mandatory=$True)]
+    [string]$extractionPath, # Path to extract to
+    [Parameter(Mandatory=$True)]
     [string]$filePath, # Exact location of the compressed file
+    [Parameter(Mandatory=$False)]
     [switch]$KeepExtracted # Determines whether extracted files are kept or deleted
 )
 "7Zip Emu-Prepper by UnluckyForSome"
@@ -11,42 +18,33 @@ param (
 "Emulator: [$emulatorPath]"
 "Emulator Arguments: [$emulatorArguments]"
 "Archive: [$filePath]"
-"Keep Extracted?: $KeepExtracted"
+"Extracting To: [$extractionPath]"
+"Keep Extracted?: [$KeepExtracted]"
 ""
-
-# Example commands:
-# PCSX2 = """--nogui"""
-# RetroArch = """"-L""" """C:/Program Files/RetroArch/cores/dolphin_libretro.dll""""
-
-# Playstation with Beetle PSX RetroArch
-# C:\Users\Joe\Documents\McWork\7zPrepper.exe "C:\PROGRA~1\7-Zip\7z.exe" "C:\PROGRA~1\RetroArch\retroarch.exe" "-L C:\PROGRA~1\RetroArch\cores\mednafen_psx_hw_libretro.dll -f" "D:\PS1\American Pool (USA).zip"
-
-# Playstation 2 with PCSX2
-# C:\Users\Joe\Documents\McWork\7zPrepper.exe "C:\PROGRA~1\7-Zip\7z.exe" "C:\PROGRA~2\PCSX2\pcsx2.exe" """--nogui""" "D:\PS2\Chess Challenger (Europe) (En,Fr,De,Es,It).7z"
 
 # Define the individual filename without the rest of the path
 $fileName = [System.IO.Path]::GetFileNameWithoutExtension("$filePath")
 
 # Start decompression in 7Zip, skip files already present and nullify the entire output
 "Starting decompression of [$filePath]..."
-& $7ZipPath "e" $filePath "-aos" | Out-Null
+& $7ZipPath "x" $filePath "-o$extractionPath" "-aos" | Out-Null
+
+# Set location to where the files have been extracted
+Set-Location -Path $extractionPath
 
 # Perform a general pass of filetypes to try and launch if emulator is not identified in the following steps
-"RetroArch Matched"
-$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+$extractedFile = Get-Item -Path "$fileName.*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
 
 # Identify PCSX2 as the emulator and search for the right type of files to launch
 if ($emulatorPath -Match 'pcsx2.exe')
 {
-"PCSX2 Matched"
-$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+$extractedFile = Get-Item -Path "$fileName.*" | Where-Object -Property Extension -Match -Value 'BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
 }
 
 # Identify RetroArch as the emulator and search for the right type of files to launch
 if ($emulatorPath -Match 'retroarch.exe')
 {
-"RetroArch Matched"
-$extractedFile = Get-Item -Path "$fileName*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
+$extractedFile = Get-Item -Path "$fileName.*" | Where-Object -Property Extension -Match -Value 'CUE|BIN|ISO' | Select-Object -Last 1 -ExpandProperty FullName
 }
 
 # Launch emulator
