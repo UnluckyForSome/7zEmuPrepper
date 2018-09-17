fa744f9b7479ce74bfa418fe11432dec761cf048
UnluckyForSome <30089567+UnluckyForSome@users.noreply.github.com>
2018-09-17 17:10:07 +0100

    Update
    Added extraction progress & now runs already-extracted files correctly!

--- 7zEmuPrepper.ps1
+++ 7zEmuPrepper.ps1
@@ -22,27 +22,47 @@ param (
 "Emulator Arguments: [$emulatorArguments]"
 "Archive: [$filePath]"
 "Extracting To: [$extractionPath]"
-"Filetype(s) To Launch: [$launchFile]"
+"Filetype To Launch: [$launchFile]"
 "Keep Extracted?: [$KeepExtracted]"
-""
 
 # Define the individual filename without the rest of the path
 $fileName = [System.IO.Path]::GetFileNameWithoutExtension("$filePath")
 
-# Start decompression in 7Zip, skip files already present and nullify the entire output
-"Starting decompression of [$filePath]..."
-& $7ZipPath "x" $filePath "-o$extractionPath" "-aos" | Out-Null
+# See if the game is already extracted
 
-# Set location to where the files have been extracted
+
+# Set location
 Set-Location -Path $extractionPath
+
+if (Test-Path $fileName"."*) {
+"Already Extracted?: [True]"
+""
 
 # Define the path of the correct file to try and launch with the emulator
 $extractedFile = Get-Item -Path "$fileName*.*" | Where-Object -Property Extension -Match -Value $launchFile | Select-Object -Last 1 -ExpandProperty FullName
-
-# Launch emulator
-"Launching emulator with [$extractedFile]..."
+"Launching [$extractedFile]..."
 Start-Process $emulatorPath -ArgumentList $emulatorArguments, """$extractedFile""" -Wait
 
+
+}
+else {
+"Already Extracted?: [False]"
+""
+
+# Start extraction in 7Zip, skip files already present and output progress
+"Extracting [$filePath]..."
+& $7ZipPath "x" $filePath "-o$extractionPath" "-aos" "-bsp1" | out-string -stream | Select-String -Pattern "\d{1,3}%" -AllMatches | ForEach-Object { $_.Matches.Value } | foreach {
+    [System.Console]::SetCursorPosition(0, [System.Console]::CursorTop) 
+    Write-Host "Progress:" $_ -NoNewLine
+}
+
+
+# Define the path of the correct file to try and launch with the emulator
+$extractedFile = Get-Item -Path "$fileName*.*" | Where-Object -Property Extension -Match -Value $launchFile | Select-Object -Last 1 -ExpandProperty FullName
+""
+"Launching [$extractedFile]..."
+Start-Process $emulatorPath -ArgumentList $emulatorArguments, """$extractedFile""" -Wait
+}
 # If "KeepExtracted" argument passed, exit before removal
 if ($KeepExtracted)
 { 
